cd4a7a92eeac19ee8106843fb0fecd533c745110
tomxiang <tomxiang@tencent.com>
2022-06-28 10:34:21 +0800

    feat:add go-interface

---
+++ hello/go-interface.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type coder interface {
+	code()
+	debug()
+}
+
+type Gopher struct {
+	language string
+}
+
+func (p Gopher) code() {
+	fmt.Printf("I am coding %s language\n", p.language)
+}
+
+func (p *Gopher) debug() {
+	fmt.Printf("I am debuging %s language\n", p.language)
+}
+
+func main() {
+	var c coder = &Gopher{"Go"}
+	c.code()
+	c.debug()
+
+	var gopher Gopher = Gopher{"Go"}
+	gopher.code()
+	/*下面两者等同*/
+	(&gopher).debug()
+	gopher.debug()
+}
